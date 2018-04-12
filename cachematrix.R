7dad096c90986a13337208d0d1f3e9538fddd3fa
Tony M <ourotheraccount.com>
2018-04-12 13:44:53 -0700

    added detailed comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,15 @@
-# makeMatrix makes a special matrix object that can cache its inverse
+# makeMatrix makes a special matrix object whose inverse can be cached using
+# cacheSolve. These functions work by taking advantage of lexical scoping in R. 
 
-#
+# The makeMatrix() func returns an object of type list() that allows access
+# to other objects (x,m) thru getters and setters. Test code using:
+#   m1<-matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+#   aMatrix<-makeMatrix(m1)
+#   m2<-cacheSolve(aMatrix)
+# The cacheSolve() func calculates m2 (the inverse of m1) or if called a
+# again returns m2 from the cached value, along with a 'getting cache' message
+# Setting a new matrix using aMatrix$set(m2) will calculate its inverse, m1
+
 makeMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -15,8 +24,6 @@ makeMatrix <- function(x = matrix()) {
        getInverse = getInverse)
 }
 
-# cacheSolve computes the inverse of the special matrix returned by makeMatrix
-# unless the inverse has already been calculated, then it retreives from the cache
 cacheSolve <- function(x, ...) {
   inv <- x$getInverse()
   if(!is.null(inv)) {
